f1f14ac24e781197e6c71ea2eb3d4cc88e71afaa
pixlewriter <83127085+pixlewriter@users.noreply.github.com>
2021-08-19 15:05:57 -0400

    Add files via upload

---
+++ Saints-of-the-Past/saints_of_the_past/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for saints_of_the_past project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saints_of_the_past.settings')
+
+application = get_asgi_application()
---
+++ Saints-of-the-Past/saints_website/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from saints_website.models import Saint, Entry
+
+admin.site.register(Saint)
+admin.site.register(Entry)
