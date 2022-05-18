2fc673952820336d6272b3ab25aba50bf0dfb549
pixlewriter <ryan@kufeldt.com>
2022-05-18 10:36:21 -0400

    Update for calendar 1.01

--- saints_website/views.py
+++ saints_website/views.py
@@ -53,6 +53,8 @@ def prayers(request):
 def calendar(request):
     saints = Saint.objects.all()
     year = datetime.now().year
+    current_day = str(datetime.now().day)
+    
     months = ["","January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"]
     month_days = ["", 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     current_month = months[datetime.now().month] 
@@ -64,19 +66,17 @@ def calendar(request):
         month_day.append(f"{i + 1}")
     if request.method == 'POST' and 'run_script' in request.POST: # link view
         from .viewfunctions import toggle
-        print(image)
         image = toggle(image)
         first_color = 'primary'
         second_color = 'secondary'
     elif request.method == 'GET' and 'run_script' in request.GET: # picture view
         from .viewfunctions import toggle
         image = False
-        print(image)
         image = toggle(image)
         first_color = 'secondary'
         second_color = 'primary'
    
-    context = {'saints':saints,'month':current_month, 'days':month_day, 'year':year, 'image_view':image, 'first_color': first_color, 'second_color': second_color}
+    context = {'saints':saints,'month':current_month, 'days':month_day, 'year':year, 'image_view':image, 'first_color': first_color, 'second_color': second_color, 'current_day': current_day}
     return render(request, 'saints_website/calendar.html', context)
 
 def switch_view(request):
